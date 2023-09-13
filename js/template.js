d9dbe1380a0e4fd0d276cf16fd342aabcaf74ab7
Tourism <BeyondLimits>
2023-09-13 10:16:31 +0800

    remove the visitor counter at the top left corner and add a beholdbataan favicon

--- js/template.js
+++ js/template.js
@@ -1,45 +1,22 @@
 // Visitor Counter
-const VisitorCounter = () =>{
-    const Eye = document.querySelector(".eye");
-    const Visitor = document.querySelector(".visitor");
-    const Close = document.querySelector(".visitor .close");
-    
-    // Events
-    Eye.addEventListener("click", openVisitor);
-    Close.addEventListener("click", closeVisitor);
-
-    function openVisitor(){
-        Visitor.classList.add('visitor-active');
-    }
-
-    function closeVisitor(){
-        Visitor.classList.remove('visitor-active');
-    }
-
-};
-
-
-VisitorCounter();
 
 // Navmenu
-const NavMenu = () =>{
-    const Bar = document.querySelector("nav .bar");
-    const NavMenu = document.querySelector("nav .navmenu");
-    const Close = document.querySelector(".navmenu .close");
+const NavMenu = () => {
+  const Bar = document.querySelector("nav .bar");
+  const NavMenu = document.querySelector("nav .navmenu");
+  const Close = document.querySelector(".navmenu .close");
 
+  // Events
+  Bar.addEventListener("click", openMenu);
+  Close.addEventListener("click", closeMenu);
 
-    // Events
-    Bar.addEventListener("click", openMenu);
-    Close.addEventListener("click", closeMenu);
+  function openMenu() {
+    NavMenu.classList.add("navmenu-active");
+  }
 
-    function openMenu(){
-        NavMenu.classList.add('navmenu-active');
-    }
-
-    function closeMenu(){
-        NavMenu.classList.remove('navmenu-active');
-    }
+  function closeMenu() {
+    NavMenu.classList.remove("navmenu-active");
+  }
 };
 
 NavMenu();
-
