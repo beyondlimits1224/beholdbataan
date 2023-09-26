35b43341c513c6ed9e11d0b8a0cbd339698ed29b
Tourism <BeyondLimits>
2023-09-26 10:08:33 +0800

    add the did you know, flash-article and upcoming-events division

---
+++ js/navmenu.js
@@ -0,0 +1,20 @@
+// Navmenu
+const NavMenu = () => {
+  const Bar = document.querySelector("nav .bar");
+  const NavMenu = document.querySelector("nav .navmenu");
+  const Close = document.querySelector(".navmenu .close");
+
+  // Events
+  Bar.addEventListener("click", openMenu);
+  Close.addEventListener("click", closeMenu);
+
+  function openMenu() {
+    NavMenu.classList.add("navmenu-active");
+  }
+
+  function closeMenu() {
+    NavMenu.classList.remove("navmenu-active");
+  }
+};
+
+NavMenu();
--- js/slidercarousel.js
+++ js/slidercarousel.js
@@ -1,24 +1,3 @@
-// Navmenu
-const NavMenu = () => {
-  const Bar = document.querySelector("nav .bar");
-  const NavMenu = document.querySelector("nav .navmenu");
-  const Close = document.querySelector(".navmenu .close");
-
-  // Events
-  Bar.addEventListener("click", openMenu);
-  Close.addEventListener("click", closeMenu);
-
-  function openMenu() {
-    NavMenu.classList.add("navmenu-active");
-  }
-
-  function closeMenu() {
-    NavMenu.classList.remove("navmenu-active");
-  }
-};
-
-NavMenu();
-
 // Hero
 let Slide = document.querySelectorAll(".slide");
 // let next = document.querySelector("");
